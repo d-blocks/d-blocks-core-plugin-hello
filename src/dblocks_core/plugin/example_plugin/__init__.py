1e836b431486f40b5ca240488b27b6ba4efde521
d-blocks-coder <g.blocks.coder@gmail.com>
2025-03-20 13:18:38 +0100

    initial commit

---
+++ src/dblocks_core/plugin/example_plugin/__init__.py
@@ -0,0 +1,18 @@
+from dblocks_core.model import plugin_model
+
+
+class ExamplePlugin(plugin_model.PluginHello):
+    """
+    This is the example plugin for d-blocks-core.
+
+    Args:
+        plugin_model (_type_): _description_
+    """
+
+    def hello(self) -> str:
+        return "Hello, world!"
+
+
+PLUGINS = [
+    ExamplePlugin(),
+]
